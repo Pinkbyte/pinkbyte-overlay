30552ac982a90345bd7ce154571f51e817ee6768
Sergey Popov <admin@pinkbyte.ru>
2012-07-03 23:47:39 +0400

    ebuild for beta version of psybnc

---
+++ net-irc/psybnc/files/psybnc-2.4_beta1-config.h
@@ -0,0 +1,56 @@
+/*
+ * Configuration file for psyBNC, created by menuconf
+ */
+
+/* Encryption */
+
+#define CRYPT
+
+/* Encryption Type*/
+
+#define BLOWFISH
+
+/* Allow Translation */
+
+#define TRANSLATE
+
+/* Allow internal network */
+
+#define INTNET
+
+/* Allow traffic logging */
+
+#define TRAFFICLOG
+
+/* Allow linkage of bouncers */
+
+#define LINKAGE
+
+/* Allow the dcc File-Functions */
+
+#define DCCFILES
+
+/* Pipe dcc Chats */
+
+#define DCCCHAT
+
+/* Allow to add more users */
+
+#define MULTIUSER
+
+/* Number of max. Users */
+
+#define MAXUSER 50
+
+/* Number of max. Connections per User */
+
+#define MAXCONN 99
+
+/* The logging level */
+
+#define LOGLEVEL 0
+
+/* SSL-Security */
+
+#define SSLSEC 2
+
